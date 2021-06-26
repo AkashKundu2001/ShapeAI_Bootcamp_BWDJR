a13155e9ddb502ce3fe1a9d77375a41085d3d5ba
AkashAnanya <anuaku2001@gmail.com>
2021-06-26 22:59:55 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by Bootcamps @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h2>Javascript and React.js</h2>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha. We covered
+        everything from scratch including Javascript, React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
